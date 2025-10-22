445dbe40c5d01f6cc7c177bc44afd1bc839e6037
Ticha23 <mutwirigachanja1@gmail.com>
2025-10-23 01:50:30 +0300

    Small changes made to the system

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const { resetPassword } = require('../controllers/userController');
-
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
